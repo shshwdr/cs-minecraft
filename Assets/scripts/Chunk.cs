bac537b2c720c337f72dead3fc12edf31973c164
shshwdr <cs.vivian04@gmail.com>
2018-11-26 02:44:20 -0800

    draw a bumping ground chunk

--- Assets/scripts/Chunk.cs
+++ Assets/scripts/Chunk.cs
@@ -24,7 +24,7 @@ public class Chunk : MonoBehaviour
             for (int z = 0; z < World.Instance.chunkWidth; z++)
             {
                 map[x, 0, z] = 1;
-                map[x, 1, z] = (byte)Random.Range(0, 1) ;
+                map[x, 1, z] = (byte)Random.Range(0, 2) ;
             }
         }
         CreateVisualMesh();
@@ -43,6 +43,18 @@ public class Chunk : MonoBehaviour
             {
                 for (int y = 0; y < World.Instance.chunkHeight; y++)
                 {
+                    if (map[x,y,z] == 0)
+                    {
+                        continue;
+                    }
+                    BuildFaces(map[x, y, z], new Vector3(x, y, z), Vector3.up, Vector3.forward, false, verts, uvs, triangles);
+                    BuildFaces(map[x, y, z], new Vector3(x + 1, y, z), Vector3.up, Vector3.forward, true, verts, uvs, triangles);
+
+                    BuildFaces(map[x, y, z], new Vector3(x, y, z), Vector3.forward, Vector3.right, false, verts, uvs, triangles);
+                    BuildFaces(map[x, y, z], new Vector3(x, y+1, z), Vector3.forward, Vector3.right, true, verts, uvs, triangles);
+
+                    BuildFaces(map[x, y, z], new Vector3(x, y, z), Vector3.up, Vector3.right, true, verts, uvs, triangles);
+                    BuildFaces(map[x, y, z], new Vector3(x, y, z+1), Vector3.up, Vector3.right, false, verts, uvs, triangles);
                 }
             }
         }
@@ -54,6 +66,40 @@ public class Chunk : MonoBehaviour
         mesh.RecalculateNormals();
         meshFilter.mesh = mesh;
         meshCollider.sharedMesh = mesh;
+    }
+
+    public virtual void BuildFaces(byte brick, Vector3 corner,Vector3 up, Vector3 right, bool reversed, List<Vector3> verts, List<Vector2> uvs, List<int> triangles)
+    {
+        int index = verts.Count;
+        //because triangles go in clockwise
+        verts.Add(corner);
+        verts.Add(corner + up);
+        verts.Add(corner + up + right);
+        verts.Add(corner + right);
+
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+
+        if (reversed) {
+            triangles.Add(index + 0);
+            triangles.Add(index + 1);
+            triangles.Add(index + 2);
+            triangles.Add(index + 2);
+            triangles.Add(index + 3);
+            triangles.Add(index + 0);
+        }
+        else
+        {
+            triangles.Add(index + 1);
+            triangles.Add(index + 0);
+            triangles.Add(index + 2);
+            triangles.Add(index + 3);
+            triangles.Add(index + 2);
+            triangles.Add(index + 0);
+        }
+        
     }
 
     // Update is called once per frame
