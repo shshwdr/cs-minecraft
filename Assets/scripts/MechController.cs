b391b6cb9c374d85c7e393eaeadf323f12c7a64c
shshwdr <cs.vivian04@gmail.com>
2018-12-07 03:09:28 -0800

    make mech as player and add move

---
+++ Assets/scripts/MechController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechController : MonoBehaviour
+{
+    public float speed = 10;
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+        rigidbody.velocity += (vert * transform.forward + horz * transform.right) * Time.deltaTime * speed;
+    }
+}
