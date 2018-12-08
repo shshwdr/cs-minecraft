6aa82c887c7e1ff15b6ecfa172b7f01fa933f050
shshwdr <cs.vivian04@gmail.com>
2018-12-08 00:25:42 -0800

    add mouse rotation

--- Assets/scripts/MechController.cs
+++ Assets/scripts/MechController.cs
@@ -1,22 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class MechController : MonoBehaviour
 {
     public float speed = 10;
     Rigidbody rigidbody;
+    [SerializeField] private MouseLook m_MouseLook;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        m_MouseLook.Init(transform, Camera.main.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
+        RotateView();
         float horz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
         rigidbody.velocity += (vert * transform.forward + horz * transform.right) * Time.deltaTime * speed;
+    }
+    private void FixedUpdate()
+    {
+        m_MouseLook.UpdateCursorLock();
+    }
+    private void RotateView()
+    {
+        m_MouseLook.LookRotation(transform, Camera.main.transform);
     }
 }
